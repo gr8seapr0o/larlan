3bece0a97c7e89872bf411108f15314f3eba0e47
gr8seapr0o <mon.to.burn@gmail.com>
2017-08-25 05:19:17 +0300

    create site from layouts

--- land/app/Http/Controllers/IndexController.php
+++ land/app/Http/Controllers/IndexController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
 
 class IndexController extends Controller
 {
     //
     public function execute(Request $request) {
+        //выбираем все данные из таблички page
+        $pages = Page::all();
+        $portfolios = Portfolio::get(array('name', 'filter', 'images'));
+        $services = Service::where('id', '<', 20)->get();
+        $peoples = People::take(3)->get();
 
-        return view('welcome');
+       // dd($pages);//колекция моделей Page
+
+
+        return view('site.index');
     }
 }
